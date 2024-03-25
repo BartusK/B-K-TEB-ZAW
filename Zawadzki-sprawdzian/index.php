be6d3dc4d5f2af809c7a8031f225bdb42f8c6890
Bartosz69 <80430440+BartusK@users.noreply.github.com>
2024-03-25 13:13:27 +0100

    Update index.php

--- Zawadzki-sprawdzian/index.php
+++ Zawadzki-sprawdzian/index.php
@@ -1,26 +1,67 @@
 <?php
-if(isset($_REQUEST['email']) && isset($_REQUEST['password'])){
+if(isset($_REQUEST['action']) && $_REQUEST['action'] == "login"){
+    
 
-}
 $email = $_REQUEST['email'];
 $password = $_REQUEST['password'];
 $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-$db = new mysqli("localhost", "root", "", "authh"); 
+$db = new mysqli("localhost", "root", "", "authh");
+
 
 $q = $db->prepare("SELECT * FROM user WHERE email = ? LIMIT 1");
 $q->bind_param("s", $email);
 $q->execute();
 $result = $q->get_result();
 $userRow = $result->fetch_assoc();
-var_dump($userRow);
+if($userRow == null){
+    echo "Failed passward or login <br>";
+} else {
+IF(password_verify($password, $userRow['passwordHash'])){
+echo "Login correct <br>";
+} else {
+echo "Failed passward or login <br>";
+}
+}
+}
+if(isset($_REQUEST['action']) && $_REQUEST['action'] == "register"){
+    $db = new mysqli("localhost", "root", "", "authh");
+    $email = $_REQUEST['email'];
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $password = $_REQUEST['password'];
+    $passwordRepeat = $_REQUEST['passwordRepeatIn'];
+    if($password = $passwordRepeat){
+    $q = $db->prepare("INSERT INTO user VALUES (NULL, ?, ?)");
+    $q->bind_param("ss", $email, $password);
+    $result = $q->execute();
+    if($result){
+        echo "Acount created";
+    } else {
+        echo "Whatever is not good";
+    }
+    } else {
+        echo "Pasward is not the same";
+    }
+}
 
 
 ?>
-<h1>Zarajestruj sie</h1>
+<h1>Zaloguj sie</h1>
 <form action="index.php" method="post">
     <label for="emailInput">Email:</label>
     <input type="email" name="email" id="emailInput">
     <label for="passwordInput">Hasło:</label>
     <input type="password" name="password" id="passwordInput">
+    <input type="hidden" name="action" value="login">
+    <input type="submit" value="Zaloguj">
+</form>
+<h1>Zarajestruj sie</h1>
+<form action="index.php" method="post">
+    <label for="emailInput">Email:</label>
+    <input type="email" name="email" id="emailInput">
+        <label for="passwordInput">Hasło:</label>
+    <input type="password" name="password" id="passwordInput">
+    <label for="passwordRepeatInput">Hasło ponowne:</label>
+    <input type="password" name="password" id="passwordRepeatInput">
+    <input type="hidden" name="action" value="register">
     <input type="submit" value="Zarejestruj">
 </form>
